baa4fecdf8345bcbaae46a2456aee4dd5515d542
Maxamed459 <maxamedmahdi459@gmail.com>
2025-08-11 19:08:58 +0300

    created postRoutes

---
+++ backend/routes/postRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  deleteQuestion,
+  getUserPosts,
+  postQuestion,
+  updateQuestion,
+} from "../controllers/postController.js";
+import { authenticate } from "../middlewares/authenticate.js";
+
+const postRouter = express.Router();
+
+postRouter.post("/create-post", authenticate, postQuestion);
+postRouter.get("/get-user-posts", authenticate, getUserPosts);
+postRouter.put("/update-post/:id", authenticate, updateQuestion);
+postRouter.delete("/delete-post/:id", authenticate, deleteQuestion);
+
+export default postRouter;
