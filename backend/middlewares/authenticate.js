4cf4b7720d8f7752eb3730069240717ceda0ee1a
Maxamed459 <maxamedmahdi459@gmail.com>
2025-08-11 19:08:14 +0300

    created middlewares

---
+++ backend/middlewares/authenticate.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/config.js";
+
+export const authenticate = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Authentication token is missing please login" });
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid authentication token" });
+  }
+};
