b1d8ef7344d3cb95ba7fb8bae9d5373135e059f9
Maxamed459 <maxamedmahdi459@gmail.com>
2025-08-12 19:26:16 +0300

    updated the dashboard sidebar menu

--- frontend/components/app-sidebar.tsx
+++ frontend/components/app-sidebar.tsx
@@ -1,26 +1,73 @@
 "use client";
-
 import * as React from "react";
-
+import {
+  AudioWaveform,
+  BookOpen,
+  CirclePlus,
+  Command,
+  File,
+  GalleryVerticalEnd,
+  Settings2,
+} from "lucide-react";
+import { NavMain } from "@/components/nav-main";
 import { NavUser } from "@/components/nav-user";
-import { Sidebar, SidebarFooter, SidebarRail } from "@/components/ui/sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarRail,
+} from "@/components/ui/sidebar";
 import { useAuth } from "@/app/context/AuthContext";
-
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "Ask question",
+      url: "/post",
+      icon: CirclePlus,
+      isActive: true,
+    },
+    {
+      title: "My questions",
+      url: "#",
+      icon: BookOpen,
+    },
+    {
+      title: "Answers",
+      url: "#",
+      icon: File,
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings2,
+    },
+  ],
+};
+interface User {
+  name: string | undefined;
+  email: string | undefined;
+  avatar: string | undefined;
+}
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { authUser } = useAuth();
-  // This is sample data.
-  const data = {
-    user: {
-      name: authUser?.username,
-      email: authUser?.email,
-      avatar: "/avatars/shadcn.jpg",
-    },
+
+  const user: User = {
+    name: authUser?.username,
+    email: authUser?.email,
+    avatar: "/avatars/shadcn.jpg",
   };
+
   return (
     <Sidebar collapsible="icon" {...props}>
-      <h1 className="text-3xl font-bold text-center py-4">Talo +</h1>
+      <div className="flex items-center justify-center py-2">
+        <img src="/dash-logo-02.png" alt="" className="max-w-[70%]" />
+      </div>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser user={user} />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
