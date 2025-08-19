7cfea8203d2012e83c1a34801928b8fa848c910a
Maxamed459 <maxamedmahdi459@gmail.com>
2025-08-19 09:59:07 +0300

    updated the formatData function

--- frontend/app/lib/foramatData.ts
+++ frontend/app/lib/foramatData.ts
@@ -2,11 +2,9 @@ export function formatMessageTime(timestamp: string) {
   if (!timestamp) return "";
   const date = new Date(timestamp);
   if (isNaN(date.getTime())) return "";
-  return date.toLocaleTimeString([], {
+  return date.toLocaleDateString([], {
     day: "2-digit",
     month: "short",
     year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
   });
 }
