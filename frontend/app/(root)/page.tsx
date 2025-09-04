e2dc69c94d86888615951d763605c2b9a71ef3ee
Maxamed459 <maxamedmahdi459@gmail.com>
2025-09-04 21:38:53 +0300

    changed welcoming text for the root page

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-6xl font-bold text-blue-600 text-center mt-20">
-        Hello, welcome to home
+        Hello, welcome to talo +
       </h1>
     </main>
   );
